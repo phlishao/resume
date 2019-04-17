bee1b8985d31959ac9f1910a74ad5e57cab21c56
phlishao <914797252@qq.com>
2019-04-17 14:47:12 +0800

    Add files via upload

---
+++ src/resume.data.js
@@ -0,0 +1,160 @@
+const resume = {
+  user: "罗绍鹏",
+  desc: ["前端工程师", "文学", "摄影"],
+  info: [
+    {
+      text: "1995年生，211本科",
+      icon: "icon-star"
+    },
+    {
+      text: "籍贯湖南,现居广州",
+      icon: "icon-location1"
+    },
+    {
+      text: "13572084305",
+      icon: "icon-phone1"
+    },
+    {
+      text: "914797252@qq.com",
+      icon: "icon-email"
+    },
+    {
+      text: "https://github.com/phlishao",
+      icon: "icon-github-fill"
+    }
+  ],
+  qrcode:"webwxgetmsgimg.jpg",
+  education: {
+    name: "教育经历",
+    subName: "Education",
+    icon: "icon-book1",
+    item: [
+      {
+        title: "西北大学",
+        timeline: "2013.9 ~ 2017.6",
+        icon: "icon-education",
+        description: [
+          {
+            title: "物联网工程（本科211）",
+            data: [
+              "大学英语四级(477)",
+              "良好的英语听说读写能力",
+              "熟悉office，excel等办公软件",
+              "了解Java，前端，及Photoshop技术"
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  experience: {
+    name: " 工作经历",
+    subName: "Experience",
+    icon: "icon-rocket",
+    item: [
+      {
+        title: "前端自学经历",
+        timeline: "2016.7 ~ 至今",
+        icon: "icon-idea",
+        description: [
+          {
+            title: "个人项目",
+            data: [
+              "Resume：一款在线简历生成器，可以在线定制，一键生成PDF简历。Github：",
+              "语雀:个人技术知识学习记录网上文档"
+            ]
+          },
+          {
+            title: "学习途径",
+            data: [
+              "基础技能：MDN官网 + 阮一峰博客 + GitHub + 掘金等",
+              "项目实战：youtube + React官网等"
+            ]
+          }
+        ]
+      },
+      {
+        title: "武汉佰钧成有限公司",
+        timeline: "2017.4-11",
+        icon: "icon-setting",
+        description: [
+          {
+            title: "Java开发工程师",
+            data: [
+              "Mysql等数据库表数据录入",
+              "上级派发相关模块MVC编码工作",
+              "相关BUG调整及后期维护功能修改"
+            ]
+          }
+        ]
+      }
+    ]
+  },
+
+  skill: {
+    name: "技能",
+    subName: "Skill",
+    icon: "icon-toolbox",
+    item: [
+      {
+        title: "web前端",
+        icon: "icon-web",
+        description: [
+          {
+            title: "HTML / CSS / JavaScript",
+            data: [
+              "熟悉已标准化的 HTML5 / CSS / ES6",
+              "熟悉JavaScript编码"
+            ]
+          },
+          {
+            title: "React技术栈",
+            data: [
+              "了解中后台前端应用的开发与设计",
+              "了解使用React技术栈：webpack+React+ReactRouter+Redux",
+              "了解蚂蚁金服的中后台前端技术体系：dva+antd+umi+antv"
+            ]
+          },
+
+          {
+            title: "前端工程实践",
+            data: [
+              "使用 gitflow工作流进行版本控制与开发",
+              "使用 Webpack 进行代码打包",
+              "使用 Eslint 进行代码静态检查 ",
+              "熟练使用css预处理器Less等"
+            ]
+          }
+        ]
+      },
+      {
+        title: "WEB 后端 ",
+        timeline: "",
+        icon: "icon-star2",
+        description: [
+          {
+            title: "Node.js",
+            data: ["了解 Node.js"]
+          }
+        ]
+      },
+      {
+        title: "自我评价",
+        timeline: "",
+        icon: "icon-idea",
+        description: [
+          {
+            title: "",
+            data: [
+              "具有一定后端经验和比较全面的HTML、CSS，JavaScript前端技术栈知识，能够独立完成各种静态页面的编写,能够解决浏览器兼容性问题",
+              "Github https://github.com/phlishao",
+              "语雀https://www.yuque.com/happy-frontend/docs",
+              "期待您的回复*^_^*"
+            ]
+          }
+        ]
+      }
+    ]
+  }
+};
+export default resume;
